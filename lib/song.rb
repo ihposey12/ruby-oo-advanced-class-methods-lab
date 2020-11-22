29d4a121783ab580dd947142e3a6d57dc041a226
Hunter Posey <ihposey@crimson.ua.edu>
2020-11-22 01:12:17 -0800

    first edit of lab

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
 class Song
   attr_accessor :name, :artist_name
+
   @@all = []
 
   def self.all
@@ -10,4 +11,37 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    new_song = Song.new
+    @@all << new_song
+    new_song
+  end
+
+  def self.new_by_name(name)
+    new_song = Song.new
+    new_song.name = name
+    new_song
+  end
+
+  def self.create_by_name(name)
+    new_song = Song.new
+    new_song.name = name
+    @@all << new_song
+    new_song
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name
+
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+
+  def new_from_file_name(file_name)
+  end
 end
